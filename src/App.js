6aaa27149f5a2df4cf0b15da6bd555127f3b847d
Shikha <shikhawebworks@gmail.com>
2023-05-05 18:01:30 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import './App.css'
+import Slider from './components/Slider'
+import Chatbot from './components/chatbot/Chatbot'
+
+const App = () => {
+  return (
+    <Router>
+      <main className="py-3">
+        <Container>
+          <div className="bot">
+            <Route path="/" component={Chatbot} exact />
+          </div>
+        </Container>
+      </main>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/chatbot/compo.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Compo = () => {
+    return (
+        <span>
+          Here is a link to the latest articles related to <a href="https://test-potatopro.com/news" target="_blank">potatos!</a>  globally Potato
+        </span>
+    );
+  }
+  
+export default Compo;
